9e69c61f591652aa6e2165443c3ab0810580e885
Zane <zaneahmed4@gmail.com>
2022-06-13 21:50:43 -0400

    remove key

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  fetch(
+    `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`
+    
+    .then((response) => response.json())
+  );
+
   return (
     <div className="App">
       <header className="App-header">
