5cc1171fcdaca7a1a0f3e9712a34b7d12de7ecaa
Khaleed Uddin <tashrif@Khaleeds-MacBook-Air-2.local>
2022-06-20 19:39:41 -0400

    updated about description

--- src/Components/VideoList.js
+++ src/Components/VideoList.js
@@ -1,27 +1,9 @@
 import {React,useState, useEffect } from "react";
 import YouTube from "react-youtube";
 import "./VideoList.css"
+import VideoCard from "./VideoCard";
 
 function VideoList({videos}) {
-
-
-
-//   const [videos, setVideos]= useState([])
-
-//   const getVideos = ()=> {
-//  fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}/video`)
-//     .then((response) => response.json())
-//     .then((response) => {
-//         getVideos(response);
-//       });
-//   };
-    
-//  useEffect(() =>{
-//    getVideos();
-//  }, []);
-
-
-
 
   return (
     <div>
