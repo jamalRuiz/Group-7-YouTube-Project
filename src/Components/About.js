dda1a6fd744338d32b028c42ccda0362b432ec9e
Zane <zaneahmed4@gmail.com>
2022-06-20 20:06:29 -0400

    add zane about

---
+++ src/Components/About.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import "./About.css"
+function About() {
+  return (
+    <div className='about'>
+
+<section className='zane'>
+        <img alt='zane'/>
+        <p>Hello! My name is Zane and I use she/they pronouns! I am an aspiring software engineer and Pursuit Fellow. I have a passion for coding and inclusivity in tech spaces for woman of color like myself.</p>
+        <a href="github.com"> https://github.com/zaneahmed1 </a>
+        <br></br>
+        <a href="linkedin.com"> www.linkedin.com/in/zaneahmed </a>
+      </section>
+
+      <section className='jamal'>
+        <img alt='jamal'/>
+        <p>Description goes here</p>
+        <a href="github.com"> add your github url </a>
+        <br></br>
+        <a href="linkedin.com"> add your linked in url</a>
+      </section>
+
+      <section className='kenyetta'>
+        <img alt='kenyetta' />
+        <p>Description goes here</p>
+        <a href="github.com"> add your github url</a>
+        <br></br>
+        <a href="linkedin.com"> add your linked in url</a>
+      </section>
+
+      <section className='khaleed'>
+        <img src="../Khaleed Uddin img.png" alt='khaleed' />
+        <p>My name is Khaleed Uddin. I just graduated with a business degree from City College of New York and I am looking to expand my skills, especially in tech world. My hobbies include sleeping, working out, watching shows, and hanging with friends.</p>
+        <a href="https://github.com/Khaleed-Uddin"> Visit my Github! </a>
+        <br></br>
+        <a href="https://www.linkedin.com/in/khaleed-uddin/"> Visit my LinkedIn! </a>
+      </section>
+
+    </div>
+  )
+}
+
+export default About
